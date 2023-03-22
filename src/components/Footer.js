7a21524ebc847f78f0acd12fb0ebf4445885e286
llcain <llcain2012@gmail.com>
2023-03-21 19:23:33 -0500

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    
+    render() { 
+        return (
+            <>
+            <div className='footer'>
+                <h1>Created by LaQuisha Cain</h1>
+            </div>
+            </>
+        );
+    }
+}
+ 
+export default Footer;
