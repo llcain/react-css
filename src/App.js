b6603023fb4f52745e82f70594bfa501b884a3d3
llcain <llcain2012@gmail.com>
2023-06-15 21:49:53 -0500

    Created gallery component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Frame from './components/Frame';
 import Car from './components/Car';
 import Navbar from './components/Navbar';
 import Users from './components/Users';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Frame />
       <Car />
       <Users />
+      <Gallery />
       <Footer />
     </>
   );
---
+++ src/components/Gallery.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Gallery extends React.Component {
+     
+    render() { 
+        return (
+            <>
+                <h1>Wildlife</h1>
+            </>
+        );
+    }
+}
+ 
+export default Gallery;
