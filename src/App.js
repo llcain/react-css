dfb03101cb4ddf97f39d6ba9caf8e6583db024d3
llcain <llcain2012@gmail.com>
2023-08-17 21:06:13 -0500

    Created About component

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Car from './components/Car';
 import Navbar from './components/Navbar';
 import Users from './components/Users';
 import Gallery from './components/Gallery';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <Navbar />
+      <About />
       <Frame />
       <Car />
       <Users />
