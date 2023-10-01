c5294928360ca14753a94a3dcb6117b4366770ae
llcain <llcain2012@gmail.com>
2023-10-01 18:02:01 -0500

    Added dynamic value in App.js

--- src/App.js
+++ src/App.js
@@ -12,8 +12,11 @@ import Gallery from './components/Gallery';
 import About from './components/About';
 
 function App() {
+  const title = "Welcome to the new blog.";
   return (
-    <>
+    <div className='App'>
+      <div className="content">
+        <h1>{ title }</h1>
       <Navbar />
       <About />
       <Frame />
@@ -21,7 +24,8 @@ function App() {
       <Users />
       <Gallery />
       <Footer />
-    </>
+      </div>
+    </div>
   );
 }
 
