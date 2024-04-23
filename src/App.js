495d8207c03eb4bb8658681677f779b19049f378
llcain <llcain2012@gmail.com>
2024-04-22 19:51:24 -0500

    Deleted some files

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './Gallery.css';
 import './About.css';
 
 import Footer from './components/Footer';
-import Frame from './components/Frame';
-import Car from './components/Car';
 import Navbar from './components/Navbar';
-import Users from './components/Users';
 import Gallery from './components/Gallery';
 import About from './components/About';
 
@@ -19,9 +16,6 @@ function App() {
         <h1>{ title }</h1>
       <Navbar />
       <About />
-      <Frame />
-      <Car />
-      <Users />
       <Gallery />
       <Footer />
       </div>
