912c6e7f7d6fd693ab62522f3f42bff800993d77
llcain <llcain2012@gmail.com>
2024-05-01 15:18:05 -0500

    Added 3 rows and two columns in grid component

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Cloud1 from '../images/cloud1.jpg';
+import Cloud2 from '../images/cloud2.jpg';
+import Cloud3 from '../images/cloud3.jpg';
 
 import  { Component } from 'react'
 
@@ -10,8 +13,40 @@ export default class Grid extends Component {
     return (
       <Container fluid>
         <Row>
-            <Col></Col>
-            <Col></Col>
+            <Col>
+                <div>
+                    <p>We have the equipment you need for your commercial needs. We have 2 universal presses.</p>
+                </div>
+            </Col>
+            <Col>
+                <div>
+                    <img src={Cloud1} alt='Cummalus Cloud'/>
+                </div>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <div>
+                    <img src={Cloud2} alt='Big cloud'/>
+                </div>
+            </Col>
+            <Col>
+                <div>
+                    <p>Look at some of the testimonials from some of our customers</p>
+                </div>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <div>
+                    <p>We have several customers we do commercial work for. You can check them out.</p>
+                </div>
+            </Col>
+            <Col>
+                <div>
+                    <img src={Cloud3} alt='Some of our customers'/>
+                </div>
+            </Col>
         </Row>
       </Container>
     )
